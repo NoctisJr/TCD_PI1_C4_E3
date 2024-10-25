1c0bcf0aa10385c16f3c64eda0c0573422414d14
DiegoQuintero2562 <133614381+DiegoQuintero2562@users.noreply.github.com>
2024-10-25 17:35:00 -0500

    cambiando colores y logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import './App.css'; // Importa el archivo de estilos CSS
+import './App.css';  // Importamos el archivo de estilos
+import logo from './assets/MOJARRA.png';  // Importamos la imagen del logo
 
 const App = () => {
   return (
     <div className="app-body">
+      
       {/* Sección del buscador */}
       <header className="search-section">
-        <h1>Buscador</h1>
-        <input type="text" placeholder="Buscar productos..." />
+        <img src={logo} alt="Logo de La ramoja" className="logo" /> {/* Logo agregado */}
+        <h1></h1>
+        <input type="text" placeholder="Buscar tours, escaladas..." />
       </header>
 
       {/* Sección de categorías */}
       <section className="categories-section">
         <h2>Categorías</h2>
         <ul>
-          <li>Categoría 1</li>
-          <li>Categoría 2</li>
-          <li>Categoría 3</li>
+          <li>Escalada</li>
+          <li>Senderismo</li>
+          <li>Aventuras</li>
         </ul>
       </section>
 
       {/* Sección de recomendaciones */}
       <section className="recommendations-section">
         <h2>Recomendaciones</h2>
-        <div className="recommendation-item">Producto recomendado 1</div>
-        <div className="recommendation-item">Producto recomendado 2</div>
+        <div className="recommendation-item">Tour de Escalada Básica</div>
+        <div className="recommendation-item">Ruta de Senderismo Familiar</div>
+        <div className="recommendation-item">Experiencia Extrema de Escalada</div>
       </section>
     </div>
   );
