669b11da9df08610a48623c5e6e09882b90ebfc1
Diego Quintero <diegoquintero941@gmail.com>
2024-11-08 15:52:35 -0500

    merge

--- backend/src/main/java/dh/backend/mojarra_tours/mapper/TourMapper.java
+++ backend/src/main/java/dh/backend/mojarra_tours/mapper/TourMapper.java
@@ -2,7 +2,6 @@ package dh.backend.mojarra_tours.mapper;
 
 import dh.backend.mojarra_tours.dto.TourDto;
 import dh.backend.mojarra_tours.entity.Tour;
-
 public class TourMapper {
     public static TourDto mapToTourDto(Tour tour){ //receives a tour, returns a tourDto
         return new TourDto(
