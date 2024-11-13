e9a719092729f9be4b137e7044242876157b9214
Diego Quintero <diegoquintero941@gmail.com>
2024-11-12 20:07:33 -0500

    cambio nombre controller

--- backend/src/main/java/dh/backend/mojarra_tours/controller/UserControllerRL.java
+++ backend/src/main/java/dh/backend/mojarra_tours/controller/UserControllerRL.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
-public class UserController {
+public class UserControllerRL {
     @Autowired
     private UserRepository userRepository;
 
