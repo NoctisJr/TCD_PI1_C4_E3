44005ca204e485b9607ac9b3d3d0ca007273c6be
Alem7u7 <soga981014g70@outlook.com>
2024-11-14 13:41:11 -0600

    Modificando isAdmin

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -35,8 +35,10 @@ const LoginForm = () => {
       axios.post(apiUrl, dataToSend)
         .then((res) => {
           console.log(res.data);
+          console.log(res.data.isAdmin);
           alert("Login exitoso!");
           sessionStorage.setItem("isLoggedIn", "true");
+          sessionStorage.setItem("isAdmin", res.data.isAdmin);
           navigate('/home');
         })
         .catch((error) => {
